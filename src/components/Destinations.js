cba07c64e5177ad573cc9ada78120c708a085913
Svitlana-G <svitlana.golubieva@gmail.com>
2021-08-02 17:33:43 +0200

    Added page Destinations

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,7 +1,19 @@
+import "./Destinations.css";
+
 const Destinations = () => {
     return (
         <section className="destinations">
-            <h1>Destinations</h1>
+            <div className="flex">
+                <h2>Featured destinations</h2>
+                <a href="/">View all &rsaquo;</a>
+            </div>
+            <div className="gallery">
+                <img src="Raja ampat.png" alt="" />
+                <img src="Fanjingshan.png" alt="" />
+                <img src="Vevey.png" alt="" />
+                <img src="Vevey.png" alt="" />
+            </div>
+
         </section>
     );
 }
